818f6253219120776a89b748f271dbd27f9f770d
Matteo Molnar <matteo.molnar@gmail.com>
2018-02-11 17:25:34 -0700

    Spacing Refactors

--- code.js
+++ code.js
@@ -4,20 +4,24 @@ function parse(txt) {
 
 function getChars(txt) {
     var count = 0;
+
     for (var i = 0; i < txt.length; i++) {
         var z = txt[i].split("");
         z.forEach(function() {
             count++;
         });
     }
+
     return count;
 }
 
 function getWords(txt) {
     var count = 0;
+
     while (count < txt.length) {
         count++;
     }
+
     return count;
 }
 
@@ -28,10 +32,12 @@ function getLines(txt) {
 function getNonEmptyLines(txt) {
     var count = 0;
     var str = txt.split(/\r\n|\r|\n/);
+
     for (var i = 0; i < str.length; i++) {
         if (str[i].trim() != "")
             count++;
     }
+
     return count;
 }
 
@@ -39,15 +45,18 @@ function getMaxLineLength(txt) {
     var maxCount = 0;
     var count = 0;
     var str = txt.split(/\r\n|\r|\n/);
+
     for (var i = 0; i < str.length; i++) {
         var x = str[i].split("");
         x.forEach(function() {
             count++
         });
+
         if (count > maxCount)
             maxCount = count
         count = 0;
     }
+
     return maxCount;
 }
 
@@ -57,6 +66,7 @@ function getAverageWordLength(charCount, wordCount) {
 
 function getPalindromes(txt) {
     var palindromes = [];
+
     for (var i = 0; i < txt.length; i++) {
         var word = txt[i].toLowerCase();
         var strArray = word.split("");
@@ -66,6 +76,7 @@ function getPalindromes(txt) {
         if (word.length > 2 && word == str)
             palindromes.push(word);
     }
+
     return palindromes;
 }
 
@@ -81,31 +92,38 @@ function uniqueArray(givenArray) {
 
 function getLongestWords(txt) {
     var words = txt;
+
     // https://stackoverflow.com/questions/10630766/sort-an-array-based-on-the-length-of-each-element
     words.sort(function(a, b){
         return b.length - a.length || a.localeCompare(b);
     });
     var uniqueWords = uniqueArray(words);
     var longestWords = [];
+
     for (var i = 0; i < 10; i++) {
         if (uniqueWords[i] != null)
             longestWords.push(uniqueWords[i].toLowerCase());
     }
+
     return longestWords;
 }
 
 function getMostFrequentWords(txt) {
     var dictionary = {};
+
     for (var i = 0; i < txt.length; i++) {
         if (dictionary[txt[i]] == null)
             dictionary[txt[i]] = 1;
+
         else if (dictionary[txt[i]] != null)
             dictionary[txt[i]]++;
     }
+
     var sortedDictionary = [];
     for (key in dictionary) {
         sortedDictionary.push([key, dictionary[key]]);
     }
+
     sortedDictionary.sort(function(a, b) {
         var nameA = a[0].toUpperCase();
         var nameB = b[0].toUpperCase();
@@ -115,22 +133,25 @@ function getMostFrequentWords(txt) {
         if (nameA > nameB) {
             return 1;
         }
-        
+
         return 0;
     });
     sortedDictionary.sort(function(a, b) {
         return b[1] - a[1];
     });
+
     var formattedDictionary = [];
     for (var x = 0; x < sortedDictionary.length; x++) {
         var name = sortedDictionary[x][0] + "(" + sortedDictionary[x][1] + ")";
         formattedDictionary.push(name);
     }
+
     var frequentWords = [];
     for (var y = 0; y < 10; y++) {
         if (formattedDictionary[y] != null)
             frequentWords.push(formattedDictionary[y].toLowerCase());
     }
+    
     return frequentWords;
 }
 
